a51be52830ba8f8825fb224c9f6c442ab455ac29
Anatolii Halas <tolikgalas@gmail.com>
2024-03-20 18:20:44 +0200

    Implemented Todo Entity: - Downloaded typeorm dependencies; - AddedTodo Tests; - Implemented Todo Entity with enum of TodoStates

--- backend/src/todo/entities/todo.entity.ts
+++ backend/src/todo/entities/todo.entity.ts
@@ -1,1 +1,29 @@
-export class Todo {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { TodoState } from './todoState.enum';
+
+@Entity()
+export class Todo {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  task: string;
+
+  @Column({
+    type: 'enum',
+    enum: TodoState,
+    default: TodoState.Await,
+  })
+  state: TodoState;
+
+  constructor(task?: string, state?: TodoState) {
+    if (task) {
+      this.task = task;
+    }
+    if (state) {
+      this.state = state;
+    } else {
+      this.state = TodoState.Await;
+    }
+  }
+}
---
+++ backend/src/todo/entities/todoState.enum.ts
@@ -0,0 +1,5 @@
+export enum TodoState {
+  Await,
+  InProgress,
+  Done,
+}
